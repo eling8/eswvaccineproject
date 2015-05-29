26fd591bc4dc44aba148cf4f7096b322007c723f
nscrimshaw <pinkmnms@gmail.com>
2015-05-29 13:55:05 -0700

    Remove error checking

--- app.rb
+++ app.rb
@@ -117,10 +117,7 @@ get '/downloadcsv' do
   voltage = params[:voltage]
   @entries = Entry.all
 
-  if date1 = "2015-05-29"
-    flash[:error] = "Please enter a valid date range"
-    redirect '/data'
-  end 
+  
 
   CSV.open('public/sample.csv', 'wb') do |csv|
     csv << ["Date 1", date1, "Date 2", date2]
