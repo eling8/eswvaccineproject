50798a1a845ecef4a8d0823c1ec77277856e13f9
Emily Ling <eling8@stanford.edu>
2015-05-30 15:35:48 -0700

    reset params after each filter

--- app.rb
+++ app.rb
@@ -62,6 +62,10 @@ get_or_post '/filter' do
       @date2 = Date.parse(params[:date2])
       @time1 = Time.parse(params[:time1])
       @time2 = Time.parse(params[:time2])
+      params[:date1] = nil
+      params[:date2] = nil
+      params[:time1] = nil
+      params[:time2] = nil
 
       @dt1 = @date1.to_datetime + @time1.seconds_since_midnight.seconds
       @dt2 = @date2.to_datetime + @time2.seconds_since_midnight.seconds
